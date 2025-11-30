d409b7124b73830eadf35ac912b2385f0f78f4b0
gulshanbauddh <gulshanbncet@gmail.com>
2025-11-30 22:17:51 +0530

    ajax complete

---
+++ Day3/highOrderLoops.js
@@ -0,0 +1,28 @@
+// const arr=[1, 2, 3, 4, 5];
+
+// // Using forEach to iterate over the array
+// arr.forEach(function(num){
+//     console.log("forEach number: " + num);
+// });
+// // Using map to create a new array with each element squared
+// const squaredArr = arr.map(function(num){
+//     return num * num;
+// });
+// console.log("Squared array: " + squaredArr);  
+// // Using filter to create a new array with only even numbers
+// const evenArr = arr.filter(function(num){
+//     return num % 2 === 0;
+// }); 
+// console.log("Even numbers array: " + evenArr);
+// // Using reduce to sum all elements in the array
+// const sum = arr.reduce(function(accumulator, currentValue){
+//     return accumulator + currentValue;
+// }, 0);
+
+// const name ="Gulshan Kumar Bauddh"
+// for (let char of name){
+//     console.log(char);
+// }
+
+// map
+let numbers = [1, 2, 3, 4, 5,6, ];
